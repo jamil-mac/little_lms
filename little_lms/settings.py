39e57e23c43a49abb4c5bd1bd4a6e1f45acab502
jamil-mac <jamil.qandimov0206@gmail.com>
2024-08-06 12:57:11 +0500

    some changes and bug fixes

--- little_lms/settings.py
+++ little_lms/settings.py
@@ -52,16 +52,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'little_lms.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASS'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASS'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
