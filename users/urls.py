803f7debc000743165ad87641afc2cfe175f097d
jamil-mac <jamil.qandimov0206@gmail.com>
2024-08-19 09:49:23 +0500

    classes grouped

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from users.views import StudentListView
+from users.views import StudentListView, ClassesListView, AllStudents
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', StudentListView.as_view(), name='students-list'),
+    path('', AllStudents.as_view(), name='all-students'),
+    path('class/', ClassesListView.as_view(), name='classes-list'),
+    path('class/<int:pk>/', StudentListView.as_view(), name='students-list'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,27 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-from users.models import StudentModel
+from users.models import StudentModel, ClassModel
 
 
-class StudentListView(ListView):
+class ClassesListView(ListView):
+    model = ClassModel
+    template_name = 'classes.html'
+    queryset = ClassModel.objects.order_by('number')
+    context_object_name = 'classes'
+
+
+class StudentListView(DetailView):
+    model = ClassModel
+    template_name = 'index.html'
+    context_object_name = 'class'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['students'] = self.object.students.order_by('surname', 'name')
+        return context
+
+
+class AllStudents(ListView):
     model = StudentModel
     template_name = 'index.html'
     queryset = StudentModel.objects.order_by('surname', 'name')
